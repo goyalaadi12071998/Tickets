a2798abbcfc03dca8dfb5f37b35f21937e762d4e
Aadesh Goyal <goyalaadesh462@gmail.com>
2020-08-17 16:43:36 +0530

    final complete

--- README.md
+++
@@ -1,7 +0,0 @@
-# Tickets
-
-Create a npm package of common module .
-Install and change the package name with @sgtickets/common
-Replace DOCKERID with your DockerID
-
-RUN npm install
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
+  console.log('Starting...');
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
@@ -31,6 +35,9 @@ const start = async () => {
     });
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
